0d0ce9ffe13ef55afa33441aa72d3eb8301a4a08
xiao <664962185@qq.com>
2025-01-28 18:04:05 +0800

    fix NodeServerUrl

--- node.py
+++ node.py
@@ -9,7 +9,7 @@ import version
 from log import log
 
 TokenUrl = 'https://www.phoenix.global/sdk/computation/user/genToken'
-NodeServerUrl = 'https://www.phoenix.global/prd/node/'
+NodeServerUrl = 'http://pnodeapi.phoenix.global/prd/node/'
 
 
 def GetToken(email,pwd):
