c754fbb6108562421bd601e025a22799e653a35c
xiao <664962185@qq.com>
2025-02-18 13:32:47 +0800

    fix heartBeat

--- contract.py
+++ contract.py
@@ -113,7 +113,11 @@ class PhoenixNodeContract():
         print("Begin start heartBeat")
         log("Begin start heartBeat")
         while True:
-            result = HeartBeat(self.key, self.node_name)
+            try:
+                result = HeartBeat(self.key, self.node_name)
+            except:
+                print("HeartBeat error")
+                log("HeartBeat error")
             if result == 1:
                 try:
                     CleanFiles()
@@ -123,7 +127,7 @@ class PhoenixNodeContract():
             else:
                 print("HeartBeat fail,please check out your local config and restart")
                 log("HeartBeat fail,please check out your local config and restart")
-                os._exit(0)
+                # os._exit(0)
             time.sleep(poll_interval)
 
     def checkVersion(self):
--- version.py
+++ version.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-VERSION = "v1.1.30"
+VERSION = "v1.1.31"
 
 if __name__ == "__main__":
    print(VERSION)
